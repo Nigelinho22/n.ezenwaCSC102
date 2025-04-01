bfd97d7ab6ccd440121b47d0d7b04871e950a2bf
Nigelinho22 <kingnjcezenwa@gmail.com>
2025-04-01 18:47:33 +0100

    (My Second Commit)

--- week-1/project_1.py
+++ week-1/project_1.py
@@ -1,5 +1,5 @@
 user = input('What is your Name Sir?:')
-print(f'Welcome to the Finance Calculator',user )
+print('Welcome to the Finance Calculator Sir',user )
 
 def simple_interest(principal, rate, time):
     si = principal * (1 + ((rate / 100) * time))
@@ -23,19 +23,3 @@ PMT = float(input("Enter Annuity Payment: "))
 print(f"Simple Interest: {simple_interest(P, R, T):.2f}")
 print(f"Compound Interest: {compound_interest(P, R, T, n):.2f}")
 print(f"Annuity Future Value: {annuity_plan(PMT, R, T, n):.2f}") 
-'''
-
-a = simple_interest(P, R, T)
-b = compound_interest(P, R, T, n)
-c = annuity_plan(PMT, R, T, n)
-
-q = int(input('Please input the calculation you want to make: 1 = Simple Interest, 2 = Compound Interest, 3 = Annuity Plan'))
-if q == 1:
-    print('Your Simple interest is:', a)
-elif q == 2:
-    print('Your Compound interest is:', b)
-elif q == 3:
-    print('Your Annuity Plan is:', c)
-else :
-    print('Invalid Input response')    
-'''
