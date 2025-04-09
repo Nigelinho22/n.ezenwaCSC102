6247c29ca1dc2edde16664ad1cd88cf96e36067c
Nigelinho22 <kingnjcezenwa@gmail.com>
2025-04-09 17:15:20 +0100

    (My Fourth Commit)

--- week-1/project_1.py
+++ week-1/project_1.py
@@ -1,6 +1,13 @@
 user = input('What is your Name Sir?:')
 print('Welcome to the Finance Calculator Sir',user )
 
+
+P = float(input("Enter Principal: "))
+R = float(input("Enter Rate of Interest: "))
+T = float(input("Enter Time in years: "))
+n = int(input("Enter number of times interest is compounded per year: "))
+PMT = float(input("Enter Annuity Payment: "))
+
 def simple_interest(principal, rate, time):
     si = principal * (1 + ((rate / 100) * time))
     return si
@@ -14,12 +21,7 @@ def annuity_plan(payment, rate, time, n):
     fv = payment * (((1 + r/n)**(n * time) - 1) / (r/n))
     return fv
 
-P = float(input("Enter Principal: "))
-R = float(input("Enter Rate of Interest: "))
-T = float(input("Enter Time in years: "))
-n = int(input("Enter number of times interest is compounded per year: "))
-PMT = float(input("Enter Annuity Payment: "))
 
-print(f"Simple Interest: {simple_interest(P, R, T):.2f}")
-print(f"Compound Interest: {compound_interest(P, R, T, n):.2f}")
-print(f"Annuity Future Value: {annuity_plan(PMT, R, T, n):.2f}") 
+print(f"The Simple Interest is: {simple_interest(P, R, T):.2f}")
+print(f"The Compound Interest is: {compound_interest(P, R, T, n):.2f}")
+print(f"The Annuity Future Value is: {annuity_plan(PMT, R, T, n):.2f}") 
